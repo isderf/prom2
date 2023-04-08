ef8d57aebd8e524f37be7cab9c3b2bc4ab1cca66
Isaac Howard <isaacrhoward@gmail.com>
2023-04-07 19:26:13 -0600

    creating webpages

--- 0_setup.sh
+++ 0_setup.sh
@@ -3,20 +3,29 @@
 sudo apt update
 sudo apt upgrade -y
 
-#install apache2
+# install apache2
 sudo apt install apache2 -y
 
-#install mysql
+# install mysql
 sudo apt install mariadb-server
 
-#install pip
+# install pip
 sudo apt install python3-pip -y
 
-#install mysql-connector
+# install mysql-connector
 pip install mysql-connector-python
-#install openpyxl for access to xlsx for pandas
+# install openpyxl for access to xlsx for pandas
 pip install openpyxl
 
 # install google api magics 
 sudo apt install python3-lxml -y
 pip install yfinance
+
+# install Flask
+pip install Flask
+# install mod_wsgi for running python web apps
+sudo apt-get install libapache2-mod-wsgi-py3
+# install python virtual environment
+sudo apt-get install python3-venv -y
+# create virtual environment
+python3 -m venv myenv
