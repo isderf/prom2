78297684f7c3ee1c328c95989decf3af47ab0fd5
Isaac Howard <isaacrhoward@gmail.com>
2023-06-15 17:19:04 -0600

    removing blueprints completely

--- projectPrometheus/blueprints/addHolding_bp.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from dao import addHolding_dao 
-
-addHolding_bp = Blueprint('addHolding', __name__)
-
-@addHolding_bp.route('/addHolding', methods=['GET'])
-def addHolding():
-    addHolding = addHolding_dao.getAllStockInfo()
-    return render_template('addHolding_form.html', results=addHolding)
